82a35af420b24c56e05810a3713a46b9ae28910d
LucaDeeken <LDeeken@aol.com>
2024-08-13 13:03:11 +0200

    fix reset bug

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 // the JavaScript file
 const body= document.querySelector("#body");
-body.style.cssText = "display:flex; flex-wrap: wrap; justify-content: center; flex-direction: column; background-color: white";
+body.style.cssText = "display:flex; flex-wrap: wrap; justify-content: center; flex-direction: column";
 
 const title = document.createElement("div");
 title.textContent= "Etch-a-Sketch!";
-title.style.cssText = "display: flex; font-size:30px; margin: 50px; justify-content: center";
+title.style.cssText = "display: flex; font-size:35px; margin: 40px; margin-top: 20px; justify-content: center; font-weight: bold";
 
 const div2 = document.createElement("div");
-div2.style.cssText = "display:flex; justify-content: center; gap: 100px";
+div2.style.cssText = "display:flex; justify-content: center; gap: 50px";
 
 const div3 = document.createElement("div");
-div3.style.cssText = "display: flex; flex-direction: column; align-content: center";
+div3.style.cssText = "display: flex; flex-direction: column; justify-content: center; gap: 200px";
 
 const fieldBox = document.querySelector(".test");
-fieldBox.style.cssText = "display: flex;flex-wrap: wrap; min-width:960px; max-width:960px; max-height:960px; min-height: 960px; margin: 0px";
+fieldBox.style.cssText = "display: flex;flex-wrap: wrap; min-width:960px; max-width:960px; max-height:960px; min-height: 960px; margin: 0px; border: solid black 4px";
 
 const button = document.createElement("button");
-button.style.cssText = "font-size:20px; padding: 30px; padding-top:20px; padding-bottom: 20px";
-button.textContent = "Apply";
+button.style.cssText = "font-size:30px; margin-top: 10px; margin-right: 55px; margin-left:55px; padding: 30px; padding-top:20px; padding-bottom: 20px; font-weight: bold; border: solid black 3px; border-radius: 10px";
+button.textContent = "Change Size";
 
 const reset = document.createElement("button");
-reset.style.cssText = "font-size:20px; margin-top: 10px; padding: 30px; padding-top:20px; padding-bottom: 20px";
+reset.style.cssText = "font-size:30px; margin-top: 10px; margin-right: 110px; margin-left:110px; padding: 30px; padding-top:20px; padding-bottom: 20px; font-weight: bold; border: solid black 3px; border-radius: 10px";
 reset.textContent = "Reset";
 
 body.appendChild(title);
@@ -50,9 +50,10 @@ for (i=0; i<256; i++) {
 
 
 let frage="";
-let x="";
-let size="";
+let x=256;
+let size=60;
 let alert1="";
+numbers = [x, size];
 
 button.addEventListener("click", () => {
     frage= prompt("How many grids per line? (100 max.)");
@@ -83,7 +84,7 @@ button.addEventListener("click", () => {
   });
 
 
-
+// reset function
   reset.addEventListener("click", () => {
     fieldBox.innerHTML = "";
     console.log(numbers);
